1bb50748f1ad40534b14307d7f08c781dd6667fd
krokodail <46492240+krokodail@users.noreply.github.com>
2023-05-12 16:36:11 +0300

    Add files via upload

--- kmp1.cpp
+++ kmp1.cpp
@@ -1,119 +1,64 @@
-#include <algorithm>
 #include <iostream>
 #include <string>
 #include <vector>
-using namespace std;
 
-//Мы читаем слева на право
-//Это функция есть переборка возможных комбинаций для строки
-//Начинаем с первой буквы, потом первая + вторая и т.д.
-//Дошли до конца строки, перешли ко второй букве, вторая + третья и т.д до конца строки
-//И всех символов в строке
+using namespace std;
 
 void func_pref(const string &stroka, vector<pair<string,int>> &preffix)
 {
 	string str;
-	for(int i = 0; i < stroka.size(); i++)
+	for(int i = 0; i < 1; i++)
 	{
-		auto t = make_pair(str = stroka[i], 0);
-		if(find(preffix.begin(), preffix.end(), t ) != preffix.end()) continue;
-		preffix.push_back(t);
+		preffix.push_back(make_pair(str = stroka[i], 0));
 		str.clear();
 		
-		for(int j = i+1; j < (stroka.size() - 1); j++)
+		for(int j = i+1; j < (stroka.size()/2); j++)
 		{
-			preffix.push_back( make_pair(preffix.back().first + stroka[j], 0) );
+			auto t = make_pair(preffix.back().first + stroka[j], 0);
+			preffix.push_back(t);
 		}
 	}
-
-	/*for(auto it = 0; it < preffix.size(); it++)
-	{
-		cout << preffix[it].first << " " << preffix[it].second << endl;
-	}*/
 }
-//Тут идея такова, что для каждой строки делаеться ипс массив
-//потом сразу же ищется количество вхождений в строку и пишется в вектор preffix
 
 void func_array_of_templates(vector< pair<string, int> > &preffix, const string &str)
 {
 	for(int i = 0; i < preffix.size(); i++)
 	{
-		int ips[preffix[i].first.size()]{0};
-		if(preffix[i].first.size() == 1)//Если в строке пары один символ -> пробегаемся по исходной строке, считая кол-во вхождений символа в строку
-		{
-			for(int k = 0; k < str.size(); k++)
-			{
-				if(preffix[i].first[0] == str[k]) ++preffix[i].second;
-			}
-			continue;
-		}
-		for(int j = 0; j < preffix[i].first.size(); j++) // этот цикл идет по строке в паре...
-		{
-			int jj = 0, ii = 1;
-			while(ii <= preffix[i].first.size())
-			{
-				if(preffix[i].first[ii] == preffix[i].first[jj]) {jj++; ips[ii] = jj; ii++;}
-				else
-				{
-					if(jj != 0) jj = ips[jj - 1];
-					else {ips[ii]=0; ii++;}
-				}
-			}
-			//Заполнили массив
-		}
-		//Теперь организуем поиск с помощью КМП
-		//Число найденных паттернов запишем в вектор с парами
 		int ii = 0, jj = 0;
 		while(ii < str.size())
 		{
 			if(preffix[i].first[jj] == str[ii]) {jj++; ii++;}
-			if(jj == preffix[i].first.size()) {++preffix[i].second; jj = ips[jj - 1];}
-			else if (ii < str.size() && preffix[i].first[jj] != str[ii])
-			{
-				if(jj != 0)
-				{
-					jj = ips[jj - 1];
-				}
-				else ii += 1;
-			}
+			else {preffix[i].second = 0; break;}
+			if(jj == preffix[i].first.size()) {++preffix[i].second; jj = 0;}
 		}
 	}
 }
 
-void show_result(const vector< pair<string, int> > &preffix, int kr)
+void show_result(const vector< pair<string, int> > &preffix, const string &str)
 {
-	int count = 1;
+	int count = 0;
 	for(int i = 0; i < preffix.size(); i++)
 	{
-		/*if(preffix[i]. second == kr)
-		{
-			cout << "Исходная строка кратна " << kr << " " << count << "-ый вариант кратности: ";
-			cout << preffix[i].first << endl;
-			++count;
-		}*/
-		cout << preffix[i].first << " " << preffix[i].second << endl;
+		if(preffix[i].second != 0){cout << "Строку " << str << " можно разбить на " << preffix[i].second 
+						 << " одинаковых подстроки "<< preffix[i].first << endl; ++count;}
 	}
+	if (count == 0) cout << endl << "Строку " << str << " нельзя разбить на одинаковые части" << endl;
 }
 
 
 void start()
 {
 	string str;//исходная строка
-	int kratnost;
 	
 	cout << "Введите строку: ";
 	getline(cin, str);
 	cout << endl;
 
-	cout << "Введите кратность: ";
-	cin >> kratnost;
-	cout << endl;
-	
 	vector <pair<string, int>> preffix;//хранит пары  префикс и количество вхождений
 	func_pref(str, preffix);//вычисляет префиксы
 	func_array_of_templates(preffix, str);//считает количество вхождений
 
-	show_result(preffix, kratnost);
+	show_result(preffix, str);
 
 }
 
